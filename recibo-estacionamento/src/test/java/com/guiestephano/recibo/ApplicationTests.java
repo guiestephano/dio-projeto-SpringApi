754c5decce75a8cffc5998632b9bb95924e06fca
Guilherme Estephano Marques Alves <guiestephano@gmail.com>
2024-05-20 15:31:09 -0300

    Inicio do Projeto

---
+++ recibo-estacionamento/src/test/java/com/guiestephano/recibo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guiestephano.recibo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
